90186781ad3ed8cd6d37f44c8f4a6cb60bf39990
Bryan Donovan <bdondo@gmail.com>
2013-07-18 08:24:12 -0700

    package.json, event-emitter test updates

--- test/memcached-eventemitters.test.js
+++ test/memcached-eventemitters.test.js
@@ -1,12 +1,23 @@
-var common = require('./common')
-  , Memcached = require('../');
+var async = require('async');
+var common = require('./common');
+var Memcached = require('../');
 
 describe("memcached event listeners", function() {
-  it("should not trigger 'EventEmitter memory leak detected' warning", function() {
-    var qty = 11;
-    for (var i = 0; i < qty; i++) {
-      var memcached = new Memcached(common.servers.single);
-      memcached.touch('key', 10, function (err) { });
-    }
-  });
+    it("should not trigger 'EventEmitter memory leak detected' warning", function(done) {
+        var qty = 11;
+        var count = 0;
+
+        function complete() {
+            return count === qty;
+        }
+
+        async.until(complete, function(cb) {
+            var memcached = new Memcached(common.servers.single);
+            memcached.touch('key', 10, function(err) {
+                memcached.end();
+                count++;
+                cb();
+            });
+        }, done);
+    });
 });
